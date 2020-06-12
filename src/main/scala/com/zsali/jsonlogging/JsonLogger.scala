53e5fe3e3c32db44a9294304869e035eb7f75ae2
Zeshan S. Ali <zeshansali@gmail.com>
2020-06-12 12:11:38 -0500

    dev: add json logging module

---
+++ src/main/scala/com/zsali/jsonlogging/JsonLogger.scala
@@ -0,0 +1,42 @@
+package com.zsali.jsonlogging
+
+package com.zsali.jsonlogging
+
+import net.logstash.logback.argument.StructuredArguments.entries
+import org.slf4j.LoggerFactory
+
+import scala.jdk.CollectionConverters._
+
+case class JsonLogger(name: String = "main") {
+  val logger = LoggerFactory.getLogger(name)
+
+  def trace(message: String, context: Map[String, String] = Map(), maybeException: Option[Exception] = None): Unit = {
+    if (logger.isTraceEnabled) {
+      logger.trace(message, entries(context.asJava), maybeException.getOrElse(None))
+    }
+  }
+
+  def debug(message: String, context: Map[String, String] = Map(), maybeException: Option[Exception] = None): Unit = {
+    if (logger.isDebugEnabled) {
+      logger.debug(message, entries(context.asJava), maybeException.getOrElse(None))
+    }
+  }
+
+  def info(message: String, context: Map[String, String] = Map(), maybeException: Option[Exception] = None): Unit = {
+    if (logger.isInfoEnabled) {
+      logger.info(message, entries(context.asJava), maybeException.getOrElse(None))
+    }
+  }
+
+  def warn(message: String, context: Map[String, String] = Map(), maybeException: Option[Exception] = None): Unit = {
+    if (logger.isWarnEnabled) {
+      logger.warn(message, entries(context.asJava), maybeException.getOrElse(None))
+    }
+  }
+
+  def error(message: String, context: Map[String, String] = Map(), maybeException: Option[Exception] = None): Unit = {
+    if (logger.isErrorEnabled) {
+      logger.error(message, entries(context.asJava), maybeException.getOrElse(None))
+    }
+  }
+}
